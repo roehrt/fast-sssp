ce73ac9d60542bb60903e5606745d79558308e54
roehrt <33495148+roehrt@users.noreply.github.com>
2025-01-20 00:33:10 +0100

    Add better fuzzer

--- fuzz.py
+++ fuzz.py
@@ -1,22 +1,40 @@
+from timeit import timeit
+
 import networkx as nx
 import random
 import subprocess
+import numpy
 
-def gen():
+random.seed(42)
+numpy.random.seed(42)
+def gen(n, m):
+    lo, hi = -10**9, 10**9
     while True:
-        G = nx.gnp_random_graph(100, 0.04, directed=True)
-        for u, v in G.edges():
-            G[u][v]['weight'] = random.randint(-3, 20)
-        if nx.negative_edge_cycle(G):
-            continue
-        yield G, nx.single_source_bellman_ford_path_length(G, source=0)
+        T = nx.dfs_tree(nx.random_labeled_tree(n), 0)
+        for u, v in T.edges():
+            T[u][v]['weight'] = random.randint(lo, hi)
+        dist = [float('inf')] * n
+        dist[0] = 0
+        for u, v in nx.dfs_edges(T, 0):
+            dist[v] = dist[u] + T[u][v]['weight']
+        for _ in range(m - (n-1)):
+            while True:
+                u, v = random.sample(range(n), 2)
+                if T.has_edge(u, v):
+                    continue
+                if not (lo <= dist[v] - dist[u] <= hi):
+                    continue
+                T.add_edge(u, v, weight=random.randint(max(lo, dist[v] - dist[u]), hi))
+                break
+        assert not nx.negative_edge_cycle(T)
+        yield T, dist
 
-for G, solution in gen():
+for G, solution in gen(5*10**4, 10**5):
     with open('fuzz.in', 'w') as f:
         f.write(f'{G.number_of_nodes()} {G.number_of_edges()}\n')
         for u, v in G.edges():
             f.write(f'{u+1} {v+1} {G[u][v]["weight"]}\n')
-        f.write(' '.join(str(solution.get(u, (2**63-1) // 2)) for u in range(G.number_of_nodes())))
+        f.write(' '.join(map(str, solution)))
     print('running...')
     with open('fuzz.in', 'r') as f:
         subprocess.run('./cmake-build-release/sssp', check=True, stdin=f)
